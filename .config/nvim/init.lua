4a411ebbf07b2d13fc046d371b653344ca6e047e
kkantadas <krishnas@kanta.gmail.com>
2022-04-23 22:19:23 +0530

    new

---
+++ .config/nvim/init.lua
@@ -0,0 +1,28 @@
+require('kk.settings')     -- point to the settings file in lua/kk
+require('kk.keymaps')      -- call keybindings
+require('kk.plugins')      -- packer pluginmanger config
+require('kk.nvim-tree')
+-- vim.cmd('colorscheme tokyonight')
+require('kk.treesitter')
+require('kk.colortheme')  -- Colortheme
+-- vim.cmd('colorscheme duskfox') ---""---
+
+   -- add transparent background 
+     require("transparent").setup({
+      enable = true, -- boolean: enable transparent
+      extra_groups = { -- table/string: additional groups that should be clear
+        -- In particular, when you set it to 'all', that means all avaliable groups
+        -- example of akinsho/nvim-bufferline.lua
+        "BufferLineTabClose",
+        "BufferlineBufferSelected",
+        "BufferLineFill",
+        "BufferLineBackground",
+        "BufferLineSeparator",
+        "BufferLineIndicatorSelected",
+      },
+    
+    -- vim.cmd -- execute *.vim skript
+      exclude = {}, -- table: groups you don't want to clear
+    })
+
+vim.cmd("source ~/.config/nvim/lua/kk/lastposition.vim")
