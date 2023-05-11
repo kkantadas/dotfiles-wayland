d6dcfc437f3ae092f9232854844ab1c14b0e6e7a
kkantadas <krishnas.kanta@gmail.com>
2023-05-11 19:56:21 +0530

    aps add

--- .config/nvim/lua/kk/nvim-tree.lua
+++ .config/nvim/lua/kk/nvim-tree.lua
@@ -36,7 +36,7 @@ local tree_cb = nvim_tree_config.nvim_tree_callback
 nvim_tree.setup {
   disable_netrw = true,
   hijack_netrw = true,
-  open_on_setup = false,
+ -- open_on_setup = false,
   ignore_ft_on_setup = {
     "startify",
     "dashboard",
@@ -84,11 +84,11 @@ nvim_tree.setup {
    -- auto_resize = true,
     mappings = {
       custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
+      --list = {
+      --  { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+      --  { key = "h", cb = tree_cb "close_node" },
+      --  { key = "v", cb = tree_cb "vsplit" },
+      --},
     },
     number = false,
     relativenumber = false,
---
+++ bin/hypr-sleep.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+ hyprctl dispatch workspace 9
+ sleep 2s &&
+ hyprctl dispatch exec systemctl suspend
+
